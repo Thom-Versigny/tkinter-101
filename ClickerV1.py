b036262601c4cd8181ab4e3d97851628a1fc1f17
Thom Versigny <Thomversigny@hotmail.com>
2022-05-10 11:02:23 +0200

    clicker moest een label zijn

--- ClickerV1.py
+++ ClickerV1.py
@@ -8,11 +8,11 @@ window.title('grabbel ton')
 def up():
     global nummer
     nummer = nummer + 1
-    buttonmiddle.config(text=nummer)
+    labelmiddle.config(text=nummer)
 def down():
     global nummer
     nummer = nummer - 1
-    buttonmiddle.config(text=nummer)
+    labelmiddle.config(text=nummer)
 
 def achtergrond():
     if nummer <= -1:
@@ -24,11 +24,11 @@ def achtergrond():
 
 
 buttonup = tkinter.Button(window, text='up', command=lambda: [up(), achtergrond()])
-buttonmiddle = tkinter.Button(window, text='0',)
+labelmiddle = tkinter.Label(window, text='0',)
 buttondown = tkinter.Button(window, text='down', command=lambda: [down(), achtergrond()])
 
 buttonup.pack(ipadx=5,ipady=5,expand=True)
-buttonmiddle.pack(ipadx=5, ipady=5, expand=True)
+labelmiddle.pack(ipadx=5, ipady=5, expand=True)
 buttondown.pack(ipadx=5, ipady=5, expand=True)
 
 window.mainloop()
